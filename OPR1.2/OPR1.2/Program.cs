cccc141d1f2a678afad5fbd6554be6e65572348b
iboblolo <167080226+iboblolo@users.noreply.github.com>
2024-04-17 19:33:42 +0700

    Add files via upload

---
+++ OPR1.2/OPR1.2/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Programm
+{
+    class LongLong
+    {
+
+        public long main;
+        public long Main
+        {
+            get { return main; }
+            set
+            {
+                main = value;
+            }
+        }
+        public ulong dop;
+        public ulong Dop
+        {
+            set
+            {
+                dop = value;
+            }
+            get { return dop; }
+        }
+        public LongLong()
+        {
+            main = 0;
+            dop = 0;
+        }
+        public LongLong(long _grad)
+        {
+            Main = _grad;
+        }
+        public LongLong(long _grad, ulong _min)
+        {
+            Main = _grad;
+            Dop = _min;
+        }
+        public override string ToString()
+        {
+            return $"M {main}; D {dop}";
+        }
+        public static LongLong operator +(LongLong a1, ulong m)
+        {
+            return new LongLong(a1.main, a1.dop + m);
+        }
+        public static LongLong operator -(LongLong a1, ulong m)
+        {
+            return new LongLong(a1.main, a1.dop - m);
+        }
+        public static bool operator <(LongLong a1, LongLong a2)
+        {
+            if (a1.main < a2.main)
+                return true;
+            else if (a1.main > a2.main)
+                return false;
+            else
+            {
+                if (a1.dop == a2.dop && a1.dop < a2.dop)
+                    return true;
+                else return false;
+            }
+        }
+        public static bool operator >(LongLong a1, LongLong a2)
+        {
+            if (a1.main > a2.main)
+                return true;
+            else if (a1.main < a2.main)
+                return false;
+            else
+            {
+                if (a1.dop == a2.dop && a1.dop > a2.dop)
+                    return true;
+                else return false;
+            }
+        }
+    }
+
+
+    class Programm
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание 2");
+
+            LongLong l = new LongLong(54, 234);
+
+
+            Console.WriteLine(l - 100);
+            Console.WriteLine(l + 100);
+
+            LongLong l2 = new LongLong(1);
+
+            Console.WriteLine(l < l2);
+            Console.WriteLine(l > l2);
+        }
+    }
+
+}
+
---
+++ OPR1/OPR1/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace Programm
+{
+    class Angle
+    {
+
+        public int grad;
+        public int Grad
+        {
+            get { return grad; }
+            set
+            {
+                grad = value;
+                while (grad >= 360)
+                {
+                    grad -= 360;
+                }
+                while (grad < 0)
+                {
+                    grad += 360;
+                }
+
+            }
+        }
+        public int min;
+        public int Min
+        {
+            set
+            {
+                min= value;
+                while (min >= 60)
+                {
+                    grad += 1;
+                    min -= 60;
+                }
+                while(min < 0)
+                {
+                    grad -= 1;
+                    min += 60;
+                }
+            }
+            get { return min; }
+            
+        }
+        public Angle()
+        {
+            grad = 0;
+            min = 0;
+
+        }
+        public Angle(int _grad)
+        {
+            Grad = _grad;
+        }
+        public Angle(int _grad, int _min)
+        {
+            Grad = _grad;
+            Min = _min;
+        }
+        public override string ToString()
+        {
+            return $"G {grad}; M {min}";
+        }
+        public double toRadians()
+        {
+            return ((grad + (min / 60)) * (3.14 / 180)) ;
+        }
+        public double GetSinus()
+        {
+            return Math.Sin(this.toRadians());
+        }
+        public static Angle operator +(Angle a1, int m)
+        {
+            return new Angle(a1.grad, a1.min + m);
+        }
+        public static Angle operator -(Angle a1, int m)
+        {
+            return new Angle(a1.grad, a1.min - m);
+        }
+        public static bool operator <(Angle a1, Angle a2)
+        {
+            if(a1.grad < a2.grad)
+                return true;
+            else if(a1.grad > a2.grad)
+                return false;
+            else
+            {
+                if (a1.min == a2.min && a1.min < a2.min)
+                    return true ;
+                else return false;
+            }
+        }
+        public static bool operator >(Angle a1, Angle a2)
+        {
+            if (a1.grad > a2.grad)
+                return true;
+            else if (a1.grad < a2.grad)
+                return false;
+            else
+            {
+                if (a1.min == a2.min && a1.min > a2.min)
+                    return true;
+                else return false;
+            }
+        }
+    }
+
+
+    class Programm
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание 1");
+
+
+            Angle angle = new Angle(366, 151);
+            Console.WriteLine(angle);
+
+            Console.WriteLine(angle.toRadians());
+
+            Angle t = angle - 100;
+            Console.WriteLine(t);
+
+            Console.WriteLine(angle > angle - 100);
+            Console.WriteLine(angle > angle + 100);
+            Console.WriteLine(angle < angle - 100);
+            Console.WriteLine(angle < angle + 100);
+            Console.WriteLine(angle.GetSinus());
+
+        }
+    }
+
+}
